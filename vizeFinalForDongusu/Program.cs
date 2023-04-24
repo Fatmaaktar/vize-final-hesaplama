7abf9c96ac4f427d4012eeac9e420c102b011f6d
Fatmaaktar <fatmaaktar214@gmail.com>
2023-04-24 16:32:21 +0300

    vizeFinalHesaplama
    Sinif mevcudu icin vize final hesaplamalari

---
+++ vizeFinalForDongusu/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vizeFinalForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int sinifMevcudu;
+            double vize, final, ort;
+            string adSoyad;
+            Console.WriteLine("Lütfen sınıf mevcudu giriniz :");
+            sinifMevcudu = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i <= sinifMevcudu; i++)
+            {
+                Console.WriteLine("Ad soyad giriniz");
+                adSoyad = Console.ReadLine();
+                Console.WriteLine("Vize notunu giriniz : ");
+                vize = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Final notunu giriniz : ");
+                final = Convert.ToDouble(Console.ReadLine());
+                ort = vize * 0.4 + final * 0.6;
+                Console.WriteLine(ort);
+
+                if (ort >= 60)
+                {
+                    Console.WriteLine(adSoyad + ": Geçti");
+                }
+                else
+                {
+                    Console.WriteLine(adSoyad + ": Kaldı");
+                }
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ vizeFinalWhileDongusu/vizeFinalWhileDongusu/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vizeFinalWhileDongusu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sinifMevcudu;
+            double vize, final, ort;
+            string adSoyad;
+            Console.WriteLine("Lütfen sınıf mevcudu giriniz :");
+            sinifMevcudu = Convert.ToInt32(Console.ReadLine());
+            int sayi = 0;
+            while (sayi<=sinifMevcudu)
+            {
+                Console.WriteLine("Ad soyad giriniz");
+                adSoyad = Console.ReadLine();
+                Console.WriteLine("Vize notunu giriniz : ");
+                vize = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Final notunu giriniz : ");
+                final = Convert.ToDouble(Console.ReadLine());
+                ort = vize * 0.4 + final * 0.6;
+                Console.WriteLine(ort);
+
+                if (ort >= 60)
+                {
+                    Console.WriteLine(adSoyad + ": Geçti");
+                }
+                else
+                {
+                    Console.WriteLine(adSoyad + ": Kaldı");
+                }
+                sayi++;
+            }
+        }
+    }
+}
